b8976e73d577646eaa53ed841c7aeccd72f5fb48
saki shishikura <saki.cec.419@gmail.com>
2022-05-08 10:18:16 +0900

    Initial Commit

---
+++ Quiz/ResultViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ResultViewController.swift
+//  Quiz
+//
+//  Created by litech on 2015/02/10.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var correctAnswer:Int = 0
+    @IBOutlet var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        resultLabel.text = "\(correctAnswer)"
+        
+        if correctAnswer == 2{
+            resultLabel.text = "神！"
+        }
+
+    }
+    
+}
